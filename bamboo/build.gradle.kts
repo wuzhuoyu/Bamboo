7576db62b68f76aa1b87a550fa6cf1a9837e90c3
leisiyu <2630940292@qq.com>
2026-01-28 10:59:08 +0800

    [version] 正式版本V0.0.8

--- bamboo/build.gradle.kts
+++ bamboo/build.gradle.kts
@@ -74,7 +74,7 @@ afterEvaluate {
            create<MavenPublication>("release"){
                group = "com.yuu.android.component"
                artifactId = "Bamboo"
-               version = "0.0.6"
+               version = "0.0.8"
                from(components["release"])
            }
         }
